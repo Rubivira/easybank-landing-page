7bf21499e7045c834c769e74507937f7eb031f37
Lucas Freitas <lucasfreitas1459@outlook.com>
2021-12-12 12:36:13 -0300

    Initial commit

---
+++ app/js/script.js
@@ -0,0 +1,26 @@
+const btnHamburger = document.querySelector('#btnHamburger')
+const body = document.querySelector('body')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+const fadeElements = document.querySelectorAll('.has-fade')
+
+btnHamburger.addEventListener('click', function () {
+    console.log('click hamburger')
+
+    if (header.classList.contains('open')) { // Close Hamburger Menu
+        header.classList.remove('open')
+        body.classList.remove('noscroll')
+        fadeElements.forEach(function (element) {
+            element.classList.remove('fade-in')
+            element.classList.add('fade-out')
+        })
+
+    } else { // Open Hamburger Menu
+        body.classList.add('noscroll')
+        header.classList.add('open')
+        fadeElements.forEach(function (element) {
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+        })
+    }
+})
